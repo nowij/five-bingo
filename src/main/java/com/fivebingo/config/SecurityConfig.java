55a75144cd823a8062a61b14bcf99f0dab61c38b
nowij <nowijdl@gmail.com>
2026-04-02 20:59:12 +0900

    프론트 도메인 변경

--- src/main/java/com/fivebingo/config/SecurityConfig.java
+++ src/main/java/com/fivebingo/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        //config.setAllowedOrigins(List.of("http://localhost:3000")); https://five-bingo-fe.vercel.app/
+        config.setAllowedOrigins(List.of("https://five-bingo-fe.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- src/main/java/com/fivebingo/config/WebSocketConfig.java
+++ src/main/java/com/fivebingo/config/WebSocketConfig.java
@@ -19,7 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
+                // .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("https://five-bingo-fe.vercel.app")
                 .withSockJS();
     }
 }
